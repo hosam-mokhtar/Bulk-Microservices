855856209a94d15efe200d4bddf02a08eaefcf2f
Ziad Ahmad <Ziad.Ahmad.intern@linkdevelopment.com>
2026-06-23 13:03:30 +0300

    Service base setup

---
+++ NutritionService/Domain/Enums/MealTypeEnum.cs
@@ -0,0 +1,7 @@
+﻿namespace NutritionService.Domain.Enums
+{
+    public enum MealTypeEnum
+    {
+        Breakfast = 0, Lunch = 1, Dinner = 2, Snack = 3
+    }
+}
---
+++ NutritionService/Infrastructure/Persistence/NutritionDbInitializer.cs
@@ -0,0 +1,17 @@
+﻿namespace NutritionService.Infrastructure.Persistence
+{
+    public class NutritionDbInitializer
+    {
+        private readonly NutritionDbContext _context;
+
+        public NutritionDbInitializer(NutritionDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedAsync()
+        {
+
+        }
+    }
+}
